ca2a5b25df3abf483c71a95ccd81415b1cec5779
Dmitry Demin <shepardiwe@gmail.com>
2018-02-05 14:06:59 +0700

    Изменил ScannerErrorKind. Теперь нельзя передать None в поля got. Для этого у UnexpectedEnd появилось поле сообщения expected. Добавил вариант UnexpectedInput(got) для случаев, когда ни одно правило сканера не может обработать ввод.

--- src/lexeme_scanner/scanner_error.rs
+++ src/lexeme_scanner/scanner_error.rs
@@ -21,31 +21,37 @@ pub enum ScannerErrorKind {
         Этот тип ошибки возникает, когда сканером ожидается какой-то определённый символ,
         а в тексте на той же позиции содержится другой.
 
-        Выводит сообщение типа `Expected: Some("<"), got: Some("%").`
+        Выводит сообщение типа `expected: Some("<"), got: Some("%").`
     */
-    ExpectedGot(char, Option<char>),
+    ExpectedGot(char, char),
     /**
         Этот тип ошибки возникает, когда сканер использует тестирующую функцию для определения
         принадлежности символа к определённой категории и символ этот тест не проходит.
 
-        Выводит сообщение типа `Symbol must be a decimal digit, got: Some("H").`
+        Выводит сообщение типа `symbol must be a decimal digit, got: "H".`
     */
-    MustBeGot(String, Option<char>),
+    MustBeGot(String, char),
     /**
         Этот тип ошибки возникает, когда сканер не готов закончить разбор из-за окончания
         ввода, однако ввод был окончен.
 
-        Выводит сообщение `Unexpected end of input.`
+        Выводит сообщение `unexpected end of input.`
     */
-    UnexpectedEnd,
+    UnexpectedEnd(Option<String>),
     /**
         Этот тип ошибки возникает, когда сканер, в процессе разбора числа, находит цифру
         равную, либо большую базису системы счисления числа. Например,
         `A` при разборе десятичных чисел или `9` при разборе восьмеричных.
 
-        Выводит сообщение типа `Digit "c" is not in 2-based radix`
+        Выводит сообщение типа `digit "c" is not in 2-based radix`
     */
     NotInRadix(char, u32),
+    /**
+        Этот тип ошибки возникает, когда сканер находит символ, который не подходит ни одному правилу.
+
+        Выводит сообщение `unexpected input`.
+    */
+    UnexpectedInput(char),
 }
 
 /// Ошибка сканирования. Содержит в себе сообщение об ошибке и указание места в тексте, где была сгенерирована эта ошибка.
@@ -55,14 +61,43 @@ pub struct ScannerError {
     pub pos: SymbolPosition,
 }
 
+impl ScannerErrorKind {
+    /// Конструирует новый `ScannerErrorKind::UnexpectedEnd` с сообщением о том, что ожидался символ
+    #[inline]
+    pub fn unexpected_end_expected_char(c: char) -> Self {
+        ScannerErrorKind::UnexpectedEnd(Some(format!("{:?}", c)))
+    }
+    /// Конструирует новый `ScannerErrorKind::UnexpectedEnd` с данным сообщением об ожидании
+    #[inline]
+    pub fn unexpected_end_expected<S: Into<String>>(msg: S) -> Self {
+        ScannerErrorKind::UnexpectedEnd(Some(msg.into()))
+    }
+    /// Конструирует новый `ScannerErrorKind::UnexpectedEnd` без сообщения
+    #[inline]
+    pub fn unexpected_end() -> Self {
+        ScannerErrorKind::UnexpectedEnd(None)
+    }
+    #[inline]
+    pub fn must_be_got<S: Into<String>>(must_be: S, got: char) -> Self {
+        ScannerErrorKind::MustBeGot(must_be.into(), got)
+    }
+}
+
 /// В реализации `Display` реализовано отображение типа ошибки в её сообщение.
 impl Display for ScannerErrorKind {
     fn fmt(&self, f: &mut Formatter) -> Result {
         match self {
-            &ScannerErrorKind::ExpectedGot(ref e, ref g) => write!(f, "expected: Some({:?}), got: {:?}", e, g),
+            &ScannerErrorKind::ExpectedGot(ref e, ref g) => write!(f, "expected: {:?}, got: {:?}", e, g),
             &ScannerErrorKind::MustBeGot(ref m, ref g) => write!(f, "symbol must be {}, got: {:?}", m, g),
-            &ScannerErrorKind::UnexpectedEnd => write!(f, "unexpected end of input"),
-            &ScannerErrorKind::NotInRadix(c, r) => write!(f, "digit {:?} is not in {}-based radix", c, r),
+            &ScannerErrorKind::UnexpectedEnd(ref e) => {
+                write!(f, "unexpected end of input")?;
+                if let &Some(ref c) = e {
+                    write!(f, ", expected: {}", c)?;
+                }
+                Ok(())
+            },
+            &ScannerErrorKind::NotInRadix(ref c, ref r) => write!(f, "digit {:?} is not in {}-based radix", c, r),
+            &ScannerErrorKind::UnexpectedInput(ref c) => write!(f, "unexpected input {:?}", c),
         }
     }
 }
