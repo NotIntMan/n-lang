b541741849ecd3bbf96816f1d414e5425b9cebd8
Dmitry Demin <shepardiwe@gmail.com>
2018-02-05 14:11:28 +0700

    Переписал алгоритм сканера под новый подход.

--- src/lexeme_scanner/scanner.rs
+++ src/lexeme_scanner/scanner.rs
@@ -1,22 +1,13 @@
 //! Сканер. Получает строку на ввод и реализует итерируемый механизм сканирования лексем в полученонй строке.
 
-use std::str::Chars;
 use std::iter::FromIterator;
 
 use super::*;
 use super::eaters::scan;
-use super::super::helpers::iter_buffer::{
-    IterBuffer,
-    PerfectBuffer,
-    IterBufferCursor,
-};
-
-pub type ScannerCursor<'a, 'b> = IterBufferCursor<'a, Chars<'b>>;
 
 /// Сканер. Получает строку на ввод и реализует итерируемый механизм сканирования лексем в полученонй строке.
 pub struct Scanner<'a> {
     source: &'a str,
-    buffer: IterBuffer<Chars<'a>>,
     position: SymbolPosition,
     finished: bool,
 }
@@ -29,26 +20,26 @@ impl<'a> Iterator for Scanner<'a>
             return None;
         }
         loop {
-            let begin = self.position.clone();
-            let mut cursor = self.buffer.cursor(begin.offset);
-            let scan_result = scan(&mut cursor);
-            let len = cursor.index - begin.offset;
+            let (scan_result, len) = match scan(self.source.as_bytes()) {
+                Ok((kind, len)) => (Ok(kind), len),
+                Err((kind, len)) => (Err(kind), len),
+            };
             let text = &self.source[..len];
+            let begin = self.position.clone();
             self.position.step_str(text);
             let kind = match scan_result {
                 Ok(kind) => kind,
                 Err(kind) => {
                     self.finished = true;
                     return Some(Err(ScannerError { kind, pos: self.position.clone() }));
-                }
+                },
             };
             self.source = &self.source[len..];
-            let pos = ItemPosition { begin, end: self.position.clone() };
             if kind.is_end() {
                 self.finished = true;
             }
             if kind.is_must_not_be_ignored() {
-                return Some(Ok(Token { kind, pos, text }));
+                return Some(Ok(Token { kind, pos: begin, text }));
             }
         }
     }
@@ -58,10 +49,8 @@ impl<'a> Scanner<'a> {
     /// Создаёт новый сканер
     #[inline]
     pub fn new(source: &'a str) -> Self {
-        let buffer = IterBuffer::new(source.chars());
         Self {
             source,
-            buffer,
             position: SymbolPosition::default(),
             finished: false,
         }
@@ -69,7 +58,7 @@ impl<'a> Scanner<'a> {
     /// Пробует собрать все лексемы (токены) в единый буфер.
     /// Если встречает ошибку, возвращает её.
     #[inline]
-    pub fn into_buffer(self) -> Result<PerfectBuffer<Token<'a>>, ScannerError> {
+    pub fn into_buffer(self) -> Result<Vec<Token<'a>>, ScannerError> {
         Result::from_iter(self)
     }
     /**
@@ -79,7 +68,7 @@ impl<'a> Scanner<'a> {
         Является композицией методов `new` и `into_buffer`.
     */
     #[inline]
-    pub fn scan(source: &'a str) -> Result<PerfectBuffer<Token<'a>>, ScannerError> {
+    pub fn scan(source: &'a str) -> Result<Vec<Token<'a>>, ScannerError> {
         Scanner::new(source).into_buffer()
     }
 }
