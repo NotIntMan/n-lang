63496a22157ecfff9271e1fab7bb5dbcb30f457c
Dmitry Demin <shepardiwe@gmail.com>
2018-02-19 10:53:32 +0700

    Обозначил несколько TODO

--- src/man_lang/expressions/definitions.rs
+++ src/man_lang/expressions/definitions.rs
@@ -100,4 +100,7 @@ pub enum Expression<'source> {
     BinaryOperation(Box<Expression<'source>>, BinaryOperator, Box<Expression<'source>>),
     PrefixUnaryOperation(PrefixUnaryOperator, Box<Expression<'source>>),
     PostfixUnaryOperation(PostfixUnaryOperator, Box<Expression<'source>>),
+    // TODO Property access expression
+    // TODO Set expression
+    // TODO Function call expression
 }
