c3b89898732c792bee71b7e1961c5e0820c8ff80
Dmitry Demin <shepardiwe@gmail.com>
2018-02-19 10:55:48 +0700

    Добавил унарные и бинарные выражения в общее правило выражений. Добавил новый модуль под более мелкие правила.

--- src/man_lang/expressions/rules/mod.rs
+++ src/man_lang/expressions/rules/mod.rs
@@ -1,6 +1,7 @@
 pub mod literals;
 pub mod binary_operations;
-pub mod unary_operators;
+pub mod unary_operations;
+pub mod others;
 
 use lexeme_scanner::Token;
 use parser_basics::{
@@ -9,11 +10,19 @@ use parser_basics::{
 };
 use super::*;
 use self::literals::literal;
+use self::binary_operations::binary_expression;
+use self::unary_operations::unary_operation;
 
-/// Функция, выполняющая разбор выражений
-pub fn expression<'token, 'source>(input: &'token [Token<'source>]) -> ParserResult<'token, 'source, Expression<'source>> {
-    alt!(input,
+parser_rule!(expression_atom(i) -> Expression<'source> {
+    alt!(i,
         literal => { |x| Expression::Literal(x) } |
         identifier_raw => { |x: &Token<'source>| Expression::Identifier(*x) }
     )
+});
+
+parser_rule!(binary_atom(i) -> Expression<'source> { unary_operation(i, expression_atom) });
+
+/// Функция, выполняющая разбор выражений
+pub fn expression<'token, 'source>(input: &'token [Token<'source>]) -> ParserResult<'token, 'source, Expression<'source>> {
+    binary_expression(input, binary_atom)
 }
