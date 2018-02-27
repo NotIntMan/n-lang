e640853d29e71f4a99afa871ca51b3f31e761290
Dmitry Demin <shepardiwe@gmail.com>
2018-02-27 15:22:25 +0700

    Убрал требования к реализации PartialEq и Debug из Assertion.

--- src/helpers/assertion.rs
+++ src/helpers/assertion.rs
@@ -2,13 +2,13 @@ pub trait Assertion<T: ?Sized = Self> {
     fn assert(&self, other: &T);
 }
 
-impl<'r, T: Assertion<R> + PartialEq<R> + ::std::fmt::Debug, R: ::std::fmt::Debug> Assertion<[R]> for [T] {
+impl<'r, T: Assertion<R>, R: ::std::fmt::Debug> Assertion<[R]> for [T] {
     fn assert(&self, other: &[R]) {
         let mut other_iter = other.iter();
         for item in self {
             let mut other_item = match other_iter.next() {
                 Some(x) => x,
-                None => return assert_eq!(self, other),
+                None => panic!("Slices should have equals size"),
             };
             item.assert(other_item);
         }
