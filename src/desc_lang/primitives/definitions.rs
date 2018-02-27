50e1114584c5eda47c563b49f71c415e43439d17
Dmitry Demin <shepardiwe@gmail.com>
2018-02-27 17:58:39 +0700

    Удалил Less-типы для типов, описывающих примитивные типы

--- src/desc_lang/primitives/definitions.rs
+++ src/desc_lang/primitives/definitions.rs
@@ -31,40 +31,6 @@ pub enum NumberType {
     },
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum NumberTypeLess {
-    Bit,
-    Boolean,
-    Integer,
-    Decimal,
-    Float,
-}
-
-impl NumberType {
-    pub fn less(&self) -> NumberTypeLess {
-        match self {
-            &NumberType::Bit {
-                size: _,
-            } => NumberTypeLess::Bit,
-            &NumberType::Boolean => NumberTypeLess::Boolean,
-            &NumberType::Integer {
-                integer_type: _,
-                unsigned: _,
-                zerofill: _,
-            } => NumberTypeLess::Integer,
-            &NumberType::Decimal {
-                size: _,
-                unsigned: _,
-                zerofill: _,
-            } => NumberTypeLess::Decimal,
-            &NumberType::Float {
-                size: _,
-                double: _,
-            } => NumberTypeLess::Float,
-        }
-    }
-}
-
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum DateTimeType {
     Date,
@@ -77,31 +43,6 @@ pub enum DateTimeType {
     Timestamp {
         precision: Option<u32>,
     },
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum DateTimeTypeLess {
-    Date,
-    Time,
-    Datetime,
-    Timestamp,
-}
-
-impl DateTimeType {
-    pub fn less(&self) -> DateTimeTypeLess {
-        match self {
-            &DateTimeType::Date => DateTimeTypeLess::Date,
-            &DateTimeType::Time {
-                precision: _,
-            } => DateTimeTypeLess::Time,
-            &DateTimeType::Datetime {
-                precision: _,
-            } => DateTimeTypeLess::Datetime,
-            &DateTimeType::Timestamp {
-                precision: _,
-            } => DateTimeTypeLess::Timestamp,
-        }
-    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -128,48 +69,9 @@ pub enum StringType {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum StringTypeLess {
-    Varchar,
-    Text,
-}
-
-impl StringType {
-    pub fn less(&self) -> StringTypeLess {
-        match self {
-            &StringType::Varchar {
-                size: _,
-                character_set: _,
-            } => StringTypeLess::Varchar,
-            &StringType::Text {
-                character_set: _,
-            } => StringTypeLess::Text,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum PrimitiveDataType {
     Number(NumberType),
     DateTime(DateTimeType),
     Year(YearType),
     String(StringType),
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum PrimitiveDataTypeLess {
-    Number,
-    DateTime,
-    Year,
-    String,
-}
-
-impl PrimitiveDataType {
-    pub fn less(&self) -> PrimitiveDataTypeLess {
-        match self {
-            &PrimitiveDataType::Number(_) => PrimitiveDataTypeLess::Number,
-            &PrimitiveDataType::DateTime(_) => PrimitiveDataTypeLess::DateTime,
-            &PrimitiveDataType::Year(_) => PrimitiveDataTypeLess::Year,
-            &PrimitiveDataType::String(_) => PrimitiveDataTypeLess::String,
-        }
-    }
 }
