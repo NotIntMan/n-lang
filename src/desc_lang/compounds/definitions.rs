45f4ad3b04a95aeed099e621402b4377aaa06f80
Dmitry Demin <shepardiwe@gmail.com>
2018-02-27 17:59:42 +0700

    Написал реализацию типажа Assertion для типа DataTypes. Привёл модуль desc_lang::compounds в надлежащий вид.

--- src/desc_lang/compounds/definitions.rs
+++ src/desc_lang/compounds/definitions.rs
@@ -1,40 +1,59 @@
 use std::collections::HashMap;
+use helpers::assertion::Assertion;
 
 use desc_lang::primitives::PrimitiveDataType;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Attribute<'a> {
-    pub name: &'a str,
-    pub arguments: Option<Vec<&'a str>>,
+pub struct Attribute<'source> {
+    pub name: &'source str,
+    pub arguments: Option<Vec<&'source str>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Field<'a> {
-    pub attributes: Vec<Attribute<'a>>,
-    pub field_type: DataType<'a>,
+pub struct Field<'source> {
+    pub attributes: Vec<Attribute<'source>>,
+    pub field_type: DataType<'source>,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct StructureDataType<'a> {
-    pub attributes: Vec<Attribute<'a>>,
-    pub fields: HashMap<&'a str, Field<'a>>,
+pub struct StructureDataType<'source> {
+    pub attributes: Vec<Attribute<'source>>,
+    pub fields: HashMap<&'source str, Field<'source>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct TupleDataType<'a> {
-    pub attributes: Vec<Attribute<'a>>,
-    pub fields: Vec<Field<'a>>,
+pub struct TupleDataType<'source> {
+    pub attributes: Vec<Attribute<'source>>,
+    pub fields: Vec<Field<'source>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum CompoundDataType<'a> {
-    Structure(StructureDataType<'a>),
-    Tuple(TupleDataType<'a>),
+pub enum CompoundDataType<'source> {
+    Structure(StructureDataType<'source>),
+    Tuple(TupleDataType<'source>),
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub enum DataType<'a> {
-    Compound(CompoundDataType<'a>),
+pub enum DataType<'source> {
+    Compound(CompoundDataType<'source>),
     Primitive(PrimitiveDataType),
-    Reference(&'a str),
+    Reference(&'source str),
+}
+
+impl<'source> Assertion<str> for DataType<'source> {
+    fn assert(&self, other: &str) {
+        let tokens = ::lexeme_scanner::Scanner::scan(other)
+            .expect("Scanner result must be ok");
+        let other_data_type = ::parser_basics::parse(
+            tokens.as_slice(),
+            ::desc_lang::compounds::data_type
+        ).expect("Parser result must be ok");
+        assert_eq!(*self, other_data_type);
+    }
+}
+
+impl<'a, 'source> Assertion<&'a str> for DataType<'source> {
+    fn assert(&self, other: &&'a str) {
+        self.assert(*other)
+    }
 }
