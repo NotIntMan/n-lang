62f347970ec55c8009e884cd117a4c3e8cc74e9b
Dmitry Demin <shepardiwe@gmail.com>
2018-04-10 15:43:26 +0700

    Перенёс и немного изменил и отладил логику поиска элементов из модуля в проект.

--- src/project_analysis/module.rs
+++ src/project_analysis/module.rs
@@ -5,17 +5,13 @@ use lexeme_scanner::Scanner;
 use parser_basics::{
     Identifier,
     parse,
-    StaticIdentifier,
 };
 use syntax_parser::modules::{
     module,
     ModuleDefinitionItem,
 };
 use syntax_parser::others::StaticPath;
-use super::source::{
-    Text,
-    TextSource,
-};
+use super::source::Text;
 use super::item::{
     Item,
     ItemRef,
@@ -32,37 +28,19 @@ pub struct Module {
 pub type ModuleRef = Arc<ReEntrantRWLock<Module>>;
 
 impl Module {
-    fn try_load_particular<S: TextSource>(source: &S, path: StaticPath) -> Result<(Arc<Text>, Vec<StaticIdentifier>), Group<SemanticError>> {
-        let path_len = path.path.len();
-        for i in 0..=path_len {
-            let module_path_len = path_len - i;
-            let module_path = &path.path[..module_path_len];
-            if let Some(text) = source.get_text(module_path) {
-                let rest_path = path.path[module_path_len..].to_vec();
-                return Ok((text, rest_path));
-            }
-        }
-        Err(Group::One(SemanticError::unresolved_item(
-            path.pos,
-            path.path,
-        )))
-    }
-    pub fn try_load<S: TextSource>(source: &S, path: StaticPath) -> Result<(ModuleRef, Vec<StaticIdentifier>), Group<SemanticError>> {
-        let (text, rest_path) = Module::try_load_particular(source, path)?;
+    pub fn try_parse(text: Arc<Text>) -> Result<ModuleRef, Group<SemanticError>> {
         let tokens = match Scanner::scan(&text.text) {
             Ok(tokens) => tokens,
             Err(error) => return Err(Group::One(SemanticError::scanner_error(error))),
         };
-        let module = match parse(&tokens, module) {
-            Ok(items) => Module::from_def(text.clone(), items),
-            Err(error_group) => return Err(Group::new(
+        match parse(&tokens, module) {
+            Ok(items) => Ok(Module::from_def(text.clone(), items)),
+            Err(error_group) => Err(Group::new(
                 error_group.extract_into_vec().into_iter()
                     .map(|item| SemanticError::parser_error(item))
                     .collect()
             )),
-        };
-        let module_ref = Arc::new(LoudRwLock::new(module, "Module was poisoned!"));
-        Ok((module_ref, rest_path))
+        }
     }
     pub fn from_def(text: Arc<Text>, items: Vec<ModuleDefinitionItem>) -> ModuleRef {
         let module_ref = Arc::new(ReEntrantRWLock::new(Module {
--- src/project_analysis/project.rs
+++ src/project_analysis/project.rs
@@ -1,15 +1,22 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 use helpers::group::Group;
-use helpers::loud_rw_lock::LoudRwLock;
+use helpers::re_entrant_rw_lock::ReEntrantRWLock;
 use parser_basics::StaticIdentifier;
-use syntax_parser::others::Path;
+use syntax_parser::others::{
+    Path,
+    StaticPath,
+};
 use super::module::{
     Module,
     ModuleRef,
 };
 use super::source::TextSource;
 use super::error::SemanticError;
+use super::item::{
+    ItemRef,
+    ItemType,
+};
 
 #[derive(Debug)]
 pub struct Project {
@@ -23,17 +30,15 @@ pub type ModulePath = Vec<StaticIdentifier>;
 pub type ModulePathSlice = [StaticIdentifier];
 
 impl Project {
-    pub fn new(root: ModuleRef) -> Self {
-        let mut modules = HashMap::with_capacity(1);
-        modules.insert(vec![], root);
+    fn new() -> Self {
         Project {
-            modules,
+            modules: HashMap::with_capacity(1),
         }
     }
     pub fn try_init<S: TextSource>(source: &S) -> Result<ProjectRef, Group<SemanticError>> {
-        let (module, _) = Module::try_load(source, Path::for_root())?;
-        let project = Project::new(module);
-        Ok(Arc::new(LoudRwLock::new(project, "Project was poisoned!")))
+        let mut project = Project::new();
+        project.find_or_load_module(source, Path::for_root())?;
+        Ok(Arc::new(ReEntrantRWLock::new(project)))
     }
     pub fn insert_module(&mut self, path: ModulePath, module: ModuleRef) {
         self.modules.insert(path, module);
@@ -46,5 +51,49 @@ impl Project {
     pub fn get_root(&self) -> ModuleRef {
         self.get_module(&[][..])
             .expect("Project do not contain root module")
+    }
+    // TODO Перенести try_load сюда для удобства чтения из источника и хранилища модулей
+    fn _find_or_load_module<S: TextSource>(&mut self, source: &S, path: &ModulePathSlice) -> Option<Result<(ModuleRef, bool), Group<SemanticError>>> {
+        if let Some(module) = self.get_module(path) {
+            return Some(Ok((module, false)));
+        }
+        let text = source.get_text(path)?;
+        match Module::try_parse(text) {
+            Ok(module) => {
+                self.insert_module(path.to_vec(), module.clone());
+                Some(Ok((module, true)))
+            },
+            Err(group) => Some(Err(group)),
+        }
+    }
+    pub fn find_or_load_module<S: TextSource>(&mut self, source: &S, path: StaticPath) -> Result<(ModuleRef, ModulePath, bool), Group<SemanticError>> {
+        let path_len = path.path.len();
+        for i in 0..=path_len {
+            let module_path_len = path_len - i;
+            let module_path = &path.path[..module_path_len];
+            match self._find_or_load_module(source, module_path) {
+                Some(Ok((module, new_flag))) => {
+                    let rest_path = path.path[module_path_len..].to_vec();
+                    return Ok((module, rest_path, new_flag));
+                }
+                Some(Err(errors)) => return Err(errors),
+                _ => {}
+            }
+        }
+        Err(Group::One(SemanticError::unresolved_item(
+            path.pos,
+            path.path,
+        )))
+    }
+    pub fn find_or_load_item<S: TextSource>(&mut self, source: &S, item_type: ItemType, path: StaticPath) -> Result<ItemRef, Group<SemanticError>> {
+        let (module, item_path, _) = self.find_or_load_module(source, path.clone())?;
+        let module = module.read();
+        match module.find_item(item_type, item_path.as_slice()) {
+            Some(item) => Ok(item),
+            None => Err(Group::One(SemanticError::unresolved_item(
+                path.pos,
+                path.path,
+            ))),
+        }
     }
 }
