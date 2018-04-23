4e397809495f59638fe48592c68ae6bb3e77d256
Dmitry Demin <shepardiwe@gmail.com>
2018-04-23 17:39:42 +0700

    Помощник as_unique потерял в обобщённости в пользу эффективности.

--- src/helpers/as_unique.rs
+++ src/helpers/as_unique.rs
@@ -1,12 +1,22 @@
 use std::hash::Hash;
 use indexmap::IndexMap;
+use parser_basics::Identifier;
 
-// TODO Semantic error on fail
-pub fn as_unique<K: Eq + Hash, V>(vec: Vec<(K, V)>) -> Result<IndexMap<K, V>, ()> {
+pub fn as_unique<K: Eq + Hash + Clone, V>(vec: Vec<(K, V)>) -> Result<IndexMap<K, V>, K> {
     let mut result = IndexMap::new();
     for (key, value) in vec {
-        if result.insert(key, value).is_some() {
-            return Err(());
+        if result.insert(key.clone(), value).is_some() {
+            return Err(key);
+        }
+    }
+    Ok(result)
+}
+
+pub fn as_unique_identifier<'source, T>(vec: Vec<(Identifier<'source>, T)>) -> Result<IndexMap<String, T>, Identifier<'source>> {
+    let mut result = IndexMap::new();
+    for (key, value) in vec {
+        if result.insert(key.text().to_string(), value).is_some() {
+            return Err(key);
         }
     }
     Ok(result)
