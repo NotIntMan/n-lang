87e8578a8773c3c6ad77ff6983660062ece1c035
Dmitry Demin <shepardiwe@gmail.com>
2018-04-25 18:11:31 +0700

    Перенёс функционал project_analysis::module_context в project_analysis::module из-за того, что модули стали дублировать слишком много функционала.

--- src/project_analysis/module.rs
+++ src/project_analysis/module.rs
@@ -1,19 +1,17 @@
 use std::sync::Arc;
 use std::fmt;
+use std::cmp;
 use indexmap::IndexMap;
 //use helpers::group::Group;
 //use helpers::re_entrant_rw_lock::ReEntrantRWLock;
 use helpers::resolve::Resolve;
 use helpers::sync_ref::SyncRef;
-use helpers::path::PathBuf;
-use lexeme_scanner::{
-//    ItemPosition,
-Scanner,
+use helpers::path::{
+    Path,
+    PathBuf,
 };
-use parser_basics::{
-//    Identifier,
-parse,
-};
+use lexeme_scanner::Scanner;
+use parser_basics::parse;
 use syntax_parser::modules::{
     module,
     ModuleDefinitionItem,
@@ -21,13 +19,13 @@ use syntax_parser::modules::{
 };
 //use syntax_parser::others::StaticPath;
 use project_analysis::{
-//    Item,
-ModuleContext,
-Text,
-SemanticItemType,
-SemanticError,
-ProjectContext,
+    Item,
+    Text,
+    SemanticItemType,
+    SemanticError,
+    ProjectContext,
 };
+
 //use super::item::{
 //    ItemBody,
 //    ItemRef,
@@ -81,33 +79,38 @@ impl Resolve<(SyncRef<PathBuf>, SyncRef<ProjectContext>)> for UnresolvedModule {
     type Result = Module;
     type Error = SemanticError;
     fn resolve(&self, ctx: &mut (SyncRef<PathBuf>, SyncRef<ProjectContext>)) -> Result<Self::Result, Vec<Self::Error>> {
-        let mut context = ModuleContext::new(ctx.0.clone(), ctx.1.clone());
-        let items_vec = match self.items.resolve(&mut context) {
-            Ok(items) => items,
-            Err(mut errors) => {
-                for error in errors.iter_mut() {
-                    error.set_text(self.text.clone());
+        let mut context = Module::new(ctx.0.clone(), ctx.1.clone());
+        {
+            let mut errors = match self.items.resolve(&mut context) {
+                Ok(_) => Vec::new(),
+                Err(mut errors) => {
+                    for error in errors.iter_mut() {
+                        error.set_text(self.text.clone());
+                    }
+                    errors
                 }
+            };
+            let mut item_names = Vec::new();
+            for (name, item) in context.items.iter() {
+                let borrowed_name = name.as_str();
+                match item_names.iter().find(|&&name| name == borrowed_name) {
+                    Some(_) => errors.push({
+                        let mut error = SemanticError::duplicate_definition(
+                            item.position,
+                            name.clone(),
+                            SemanticItemType::Definition,
+                        );
+                        error.set_text(self.text.clone());
+                        error
+                    }),
+                    None => item_names.push(borrowed_name),
+                }
+            }
+            if !errors.is_empty() {
                 return Err(errors);
             }
-        };
-        let mut items = IndexMap::new();
-        let mut errors = Vec::new();
-        for (name, item) in items_vec {
-            let pos = item.position;
-            if items.insert(name.clone(), item).is_some() {
-                errors.push(SemanticError::duplicate_definition(
-                    pos,
-                    name,
-                    SemanticItemType::Definition,
-                ))
-            }
         }
-        Ok(Module {
-            items,
-            path: ctx.0.clone(),
-            project: ctx.1.clone(),
-        })
+        Ok(context)
     }
 }
 
@@ -116,6 +119,64 @@ pub struct Module {
     items: IndexMap<String, ModuleDefinitionItem>,
     path: SyncRef<PathBuf>,
     project: SyncRef<ProjectContext>,
+    imported: Vec<SyncRef<Module>>,
+}
+
+impl Module {
+    #[inline]
+    pub fn new(path: SyncRef<PathBuf>, project: SyncRef<ProjectContext>) -> Self {
+        Module {
+            items: IndexMap::new(),
+            path,
+            project,
+            imported: Vec::new(),
+        }
+    }
+    #[inline]
+    pub fn put_item(&mut self, name: &str, value: ModuleDefinitionItem) {
+        self.items.insert(name.into(), value);
+    }
+    #[inline]
+    fn get_item_inside_module<'a>(&self, mut path: Path<'a>) -> Option<(&ModuleDefinitionItem, Path<'a>)> {
+        let item = self.items.get(path.pop_left()?)?;
+        Some((item, path))
+    }
+    pub fn get_item(&self, path: Path, search_route: &mut Vec<SyncRef<Module>>) -> Option<SyncRef<Item>> {
+        match self.get_item_inside_module(path) {
+            Some((item_def, rest_path)) => item_def.value.get_item(rest_path, search_route),
+            None => {
+                for module in self.imported.iter() {
+                    if let Some(item) = module.get_item(path, search_route) {
+                        return Some(item);
+                    }
+                }
+                None
+            }
+        }
+    }
+    #[inline]
+    pub fn resolve_import(&self, path: Path) -> Option<SyncRef<Item>> {
+        self.project.resolve_item(path)
+    }
+    #[inline]
+    pub fn inject_import_module(&mut self, module: SyncRef<Module>) {
+        if !module.has_same_ref_in(&self.imported) {
+            self.imported.push(module);
+        }
+    }
+}
+
+impl SyncRef<Module> {
+    pub fn get_item(&self, path: Path, search_route: &mut Vec<SyncRef<Module>>) -> Option<SyncRef<Item>> {
+        if self.has_same_ref_in(search_route) {
+            return None;
+        }
+        search_route.push(self.clone());
+        if path.is_empty() {
+            return Some(SyncRef::new(Item::module_ref(self.clone())));
+        }
+        self.read().get_item(path, search_route)
+    }
 }
 
 impl fmt::Debug for Module {
@@ -126,6 +187,25 @@ impl fmt::Debug for Module {
             .finish()
     }
 }
+
+impl cmp::PartialEq for Module {
+    fn eq(&self, other: &Module) -> bool {
+        self.project.is_same_ref(&other.project)
+            &&
+            self.path == other.path
+            &&
+            self.items == other.items
+    }
+    fn ne(&self, other: &Module) -> bool {
+        !self.project.is_same_ref(&other.project)
+            ||
+            self.path != other.path
+            ||
+            self.items != other.items
+    }
+}
+
+impl cmp::Eq for Module {}
 
 //#[derive(Debug, Clone, PartialEq, Eq)]
 //pub struct ModuleRef(pub Arc<ReEntrantRWLock<Module>>);
--- src/project_analysis/module_context.rs
+++ src/project_analysis/module_context.rs
@@ -1,70 +1,89 @@
-use std::collections::HashMap;
-use std::fmt;
-use helpers::sync_ref::SyncRef;
-use helpers::path::PathBuf;
-use syntax_parser::others::ItemPath;
-use project_analysis::{
-    Item,
-    SemanticError,
-    ProjectContext,
-};
-
-pub struct ModuleContext {
-    items: HashMap<String, SyncRef<Item>>,
-    module_path: SyncRef<PathBuf>,
-    project: SyncRef<ProjectContext>,
-}
-
-pub struct ErrorTypeNotFound;
-
-impl ModuleContext {
-    #[inline]
-    pub fn new(module_path: SyncRef<PathBuf>, project: SyncRef<ProjectContext>) -> Self {
-        ModuleContext {
-            items: HashMap::new(),
-            module_path,
-            project,
-        }
-    }
-    pub fn put_item(&mut self, name: &str, value: Item) -> SyncRef<Item> {
-        let refer = SyncRef::new(value);
-        self.items.insert(name.into(), refer.clone());
-        refer
-    }
-    pub fn get_item(&mut self, path: &ItemPath) -> Result<SyncRef<Item>, Vec<SemanticError>> {
-        let (search_name, rest_path) = path.path.as_path().pop_left();
-        let search_name = match search_name {
-            Some(search_name) => search_name,
-            None => unimplemented!(),
-        };
-        for (item_name, item) in self.items.iter() {
-            if item_name == search_name {
-                match item.get_item(rest_path) {
-                    Some(item) => return Ok(item),
-                    None => break,
-                }
-            }
-        }
-        Err(vec![
-            SemanticError::unresolved_item(path.pos, path.path.clone()),
-        ])
-    }
-//    pub fn put_type(&mut self, name: &str, data_type: DataType<'static>) {
-//        self.types.insert(name.to_string(), data_type);
-//    }
-//    pub fn get_type(&mut self, name: &str) -> Result<DataType<'static>, ErrorTypeNotFound> {
-//        match self.types.get(name) {
-//            Some(res) => Ok(res.clone()),
-//            None => Err(ErrorTypeNotFound),
+//use std::collections::HashMap;
+//use std::fmt;
+//use helpers::sync_ref::SyncRef;
+//use helpers::path::{
+//    Path,
+//    PathBuf,
+//};
+//use syntax_parser::others::ItemPath;
+//use project_analysis::{
+//    Item,
+//    Module,
+//    SemanticError,
+//    ProjectContext,
+//};
+//
+//pub struct ModuleContext {
+//    items: HashMap<String, SyncRef<Item>>,
+//    module_path: SyncRef<PathBuf>,
+//    project: SyncRef<ProjectContext>,
+//}
+//
+//pub struct ErrorTypeNotFound;
+//
+//impl ModuleContext {
+//    #[inline]
+//    pub fn new(module_path: SyncRef<PathBuf>, project: SyncRef<ProjectContext>) -> Self {
+//        ModuleContext {
+//            items: HashMap::new(),
+//            module_path,
+//            project,
 //        }
 //    }
-}
-
-impl fmt::Debug for ModuleContext {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("ModuleContext")
-            .field("items", &self.items)
-            .field("module_path", &self.module_path)
-            .finish()
-    }
-}
+//    pub fn put_item(&mut self, name: &str, value: SyncRef<Item>) {
+//        self.items.insert(name.into(), value);
+//    }
+//    pub fn new_item(&mut self, name: &str, value: Item) -> SyncRef<Item> {
+//        let refer = SyncRef::new(value);
+//        self.put_item(name, refer.clone());
+//        refer
+//    }
+//    pub fn get_item(&mut self, path: &ItemPath) -> Result<SyncRef<Item>, Vec<SemanticError>> {
+//        let mut rest_path = path.path.as_path();
+//        let search_name = match rest_path.pop_left() {
+//            Some(search_name) => search_name,
+//            None => unimplemented!(),
+//        };
+//        for (item_name, item) in self.items.iter() {
+//            if item_name == search_name {
+//                match item.get_item(rest_path) {
+//                    Some(item) => return Ok(item),
+//                    None => break,
+//                }
+//            }
+//        }
+//        Err(vec![
+//            SemanticError::unresolved_item(path.pos, path.path.clone()),
+//        ])
+//    }
+//    #[inline]
+//    pub fn module_path(&self) -> &SyncRef<PathBuf> {
+//        &self.module_path
+//    }
+//    #[inline]
+//    pub fn get_module(&self, path: Path) -> Option<SyncRef<Module>> {
+//        self.project.get_module(path)
+//    }
+//    #[inline]
+//    pub fn resolve_import(&self, path: Path) -> Option<SyncRef<Item>> {
+//        self.project.resolve_item(path)
+//    }
+////    pub fn put_type(&mut self, name: &str, data_type: DataType<'static>) {
+////        self.types.insert(name.to_string(), data_type);
+////    }
+////    pub fn get_type(&mut self, name: &str) -> Result<DataType<'static>, ErrorTypeNotFound> {
+////        match self.types.get(name) {
+////            Some(res) => Ok(res.clone()),
+////            None => Err(ErrorTypeNotFound),
+////        }
+////    }
+//}
+//
+//impl fmt::Debug for ModuleContext {
+//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//        f.debug_struct("ModuleContext")
+//            .field("items", &self.items)
+//            .field("module_path", &self.module_path)
+//            .finish()
+//    }
+//}
